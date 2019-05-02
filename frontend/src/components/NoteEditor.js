628d56bf12a8c2d5b89edcd65e4519947c20e3e1
louieogrady <l.ogrady@gmail.com>
2019-05-02 15:59:17 +0100

    almost there with the edit

--- frontend/src/components/NoteEditor.js
+++ frontend/src/components/NoteEditor.js
@@ -7,7 +7,7 @@ class NoteEditor extends Component {
         <input type="text" name="title" defaultValue={this.props.selectedNote.title}/>
         <textarea name="body" defaultValue={this.props.selectedNote.body}/>
         <div className="button-row">
-          <input className="button" type="submit" value="Save" />
+          <input className="button" type="submit" value="Save" onClick={(event) => this.props.switchPatchEdit(this.props.selectedNote.id, event.target.name.value, event.target.body.value)}/>
           <button type="button" onClick={() => this.props.switchNoteEditorRenderCondition()} >Cancel</button>
         </div>
       </form>
@@ -16,3 +16,8 @@ class NoteEditor extends Component {
 }
 
 export default NoteEditor;
+
+
+//onSubmit={this.props.patchEdit}
+
+// if we cannot get the value from the event.target - then lets setup state and assign an onchange to the inputs and put the input into state - send that state back to the content container for posting to the api
